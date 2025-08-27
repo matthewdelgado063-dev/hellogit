e7936983fc72cdaec647502e05339a594fc99a00
matthewdelgado063 <matthewdelgado063@localhost>
2025-08-27 13:03:15 -0500

    added my major

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("My name is matthew"); 
+		System.out.print("My major is computer science");
 		
 	}
 
